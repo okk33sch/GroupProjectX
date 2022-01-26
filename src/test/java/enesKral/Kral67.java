e610f642dd71f44c7f0a808ce48e3c13b30d1d61
enes <enesuzun2020@gmail.com>
2022-01-26 00:49:57 -0500

    second commit

--- src/test/java/enesKral/Kral67.java
+++ src/test/java/enesKral/Kral67.java
@@ -1,7 +1,7 @@
 package enesKral;
 
 public class Kral67 {
-   // public static void main(String[] args) {
-    //    System.out.println("Efsane geri dondu");
+    public static void main(String[] args) {
+        //    System.out.println("Efsane geri dondu");
     }
-
+}
