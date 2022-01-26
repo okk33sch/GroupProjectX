26df094f1264e4ab96cad22d82f9bdc0ba0e4402
enes <enesuzun2020@gmail.com>
2022-01-26 01:46:13 -0500

    uyku commit

--- src/test/java/enesKral/Kral67.java
+++ src/test/java/enesKral/Kral67.java
@@ -2,6 +2,7 @@ package enesKral;
 
 public class Kral67 {
     public static void main(String[] args) {
-        //    System.out.println("Efsane geri dondu");
+          System.out.println("Efsane geri dondu");
+        System.out.println("efsane uyuyor");
     }
 }
